c4059837e649d3259b4fa02e7dbcff6a45a02596
matedge <mat.edge@gmail.com>
2017-07-13 15:40:17 +1000

    css practice

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "200px";
+    console.log("nav");
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    console.log("nav2");
 }
